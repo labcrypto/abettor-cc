5ee86d3f682fe2909ada07d21bd979cc1b49eb74
Kamran Amini <kam.cpp@gmail.com>
2016-04-12 21:18:39 +0430

    Date helper mehods have been added.

---
+++ date/helper.cc
@@ -0,0 +1,13 @@
+#include <naeem++/date/helper.h>
+
+
+namespace naeem {
+  namespace date {
+    namespace helper {
+      std::string 
+      GetCurrentTime() {
+        return "";
+      }
+    }
+  }
+}
